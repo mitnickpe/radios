d8a3c2690e3f629db45a0a329e29be8f9def97cc
Michael <michaelcr_20@hotmail.com>
2023-04-02 00:25:18 -0500

    Update Library.js

--- src/components/Library.js
+++ src/components/Library.js
@@ -13,7 +13,7 @@ const Library = ({
 }) => {
     return (
         <div className={`library ${libraryStatus ? 'active-library' : "" }`}>
-            <h2>Library</h2>
+            <h2>Canciones</h2>
             <div className="library-songs">
                 {songs.map(song => (
                     <LibrarySong 
