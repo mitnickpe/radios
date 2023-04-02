668b43903def8d2f4e07545fa02798744a2023e8
Michael <michaelcr_20@hotmail.com>
2023-04-02 00:56:11 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -9,7 +9,7 @@ function chillHop() {
             cover: "https://png.pngtree.com/png-vector/20190726/ourlarge/pngtree-modern-square-cool-music-for-party-lights-png-image_1629696.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
-            audio: "http://pureplay.cdnstream1.com/6011_128.mp3", 
+            audio: "https://od.lk/d/179154931_2Ixol/BG.mp3", 
             active: true,
         },
         {
